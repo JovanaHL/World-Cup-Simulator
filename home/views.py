6f4c5102c57d7e19be4dca1b37272f1df6b49c7d
JovanaHL <jyhe223@uky.edu>
2023-02-12 23:46:08 -0500

    Test view and model updates

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Team
 
 def home(request):
+    team = Team.objects.all().values()
     template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    context = {
+        'team': team,
+    }
+    return HttpResponse(template.render(context, request))
 
